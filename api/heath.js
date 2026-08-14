9bc441735ccbfad291dc88e3d1698991f76be284
432upproducoes <432up.producoes@gmail.com>
2026-08-14 17:07:23 -0300

    Create heath.js

---
+++ api/heath.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  try {
+    // Endereço testando a tabela 'monitoramento'
+    const response = await fetch('https://paetkspbfejtjjkngqej.supabase.co/rest/v1/monitoramento?limit=1', {
+      headers: {
+        'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBhZXRrc3BiZmVqdGpqa25ncWVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MDU2OTgsImV4cCI6MjA4NjQ4MTY5OH0.IiYweZ2g3bP7b0o7VvBW5LLb6d1oHtSNFUZlVkIsdsA',
+        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBhZXRrc3BiZmVqdGpqa25ncWVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MDU2OTgsImV4cCI6MjA4NjQ4MTY5OH0.IiYweZ2g3bP7b0o7VvBW5LLb6d1oHtSNFUZlVkIsdsA'
+      }
+    });
+
+    if (response.ok) {
+      return res.status(200).send('ONLINE');
+    }
+    return res.status(500).send('OFFLINE');
+  } catch (error) {
+    return res.status(500).send('OFFLINE');
+  }
+}
