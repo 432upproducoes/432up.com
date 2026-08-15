47fe851e38fcbe21ac80b3ebc68d5e2abae17141
432upproducoes <432up.producoes@gmail.com>
2026-08-14 22:03:23 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -351,7 +351,7 @@ async function handleProgressFormSubmit(e) {
 
 
 
-await dispararLeadTelegram(name, contact, msg || 'Contato pela calculadora');
+await dispararLeadTelegram(name, contact, msg || 'Contato pelo Site');
 
 
    
